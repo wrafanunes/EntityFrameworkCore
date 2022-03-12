53121e80dc3da3a3d6c11deefd8d1b12d1c29ee4
wrafanunes <wrsnunes@gmail.com>
2022-03-11 22:26:31 -0300

    adding a migration to validate the types and length of properties

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,22 @@ namespace EFDataAccessLibrary.Models
     public class Address
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string StreetAddress { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string State { get; set; }
+
+        [Required]
+        [MaxLength(9)]
+        [Column(TypeName = "varchar(10)")]
         public string ZipCode { get; set; }
     }
 }
--- EFDataAccessLibrary/Models/Email.cs
+++ EFDataAccessLibrary/Models/Email.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace EFDataAccessLibrary.Models
     public class Email
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string EmailAddress { get; set; }
     }
 }
