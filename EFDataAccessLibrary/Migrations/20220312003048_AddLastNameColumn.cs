9b66b004629d7405543a842f00bfee883f159a6d
wrafanunes <wrsnunes@gmail.com>
2022-03-11 22:06:19 -0300

    adding the first two migrations
    - initial migration to create database
    - migration to add the column LastName to the People table

---
+++ EFDataAccessLibrary/Migrations/20220312003048_AddLastNameColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccessLibrary.Migrations
+{
+    public partial class AddLastNameColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "People");
+        }
+    }
+}
