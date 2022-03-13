6cfbb950b4b025901849ae6458b57e79aa37d21d
wrafanunes <wrsnunes@gmail.com>
2022-03-13 20:44:51 -0300

    add a json with multiple data and creating method to add to db
    - add a file called generated.json to the front end project to save those data to the database
    - create a method to verify if the people table is empty and, if so, insert the
      data from the generated.json to the db
    Please enter the commit message for your changes. Lines starting

--- EntityFrameworkCoreWeb/Pages/Index.cshtml.cs
+++ EntityFrameworkCoreWeb/Pages/Index.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EFDataAccessLibrary.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreWeb.Pages
@@ -11,15 +15,33 @@ namespace EntityFrameworkCoreWeb.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _peopleContext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PeopleContext context)
         {
             _logger = logger;
+            _peopleContext = context;
         }
 
         public void OnGet()
         {
+            LoadSampleData();
+            var people = _peopleContext.People
+                .Include(a => a.Addresses)
+                .Include(e => e.EmailAddresses)
+                .ToList();
+        }
 
+        private void LoadSampleData()
+        {
+            if (_peopleContext.People.Count() == 0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _peopleContext.AddRange(people);
+                _peopleContext.SaveChanges();
+                return;
+            }
         }
     }
 }
